33f3b0b11edff202f568550b0ba91c2cdbfc199f
luojia65 <me@luojia.cc>
2019-11-17 22:09:03 +0800

    White light

--- examples/vega-lite-led.rs
+++ examples/vega-lite-led.rs
@@ -10,7 +10,11 @@ fn main() -> ! {
     let cp = pac::Peripherals::take().unwrap();
     let mut pcc0 = cp.PCC0.constrain();
     let gpioa = (cp.GPIOA, cp.PORTA).split(&mut pcc0.porta).unwrap();
+    let mut pta22 = gpioa.pta22.into_push_pull_output();
+    let mut pta23 = gpioa.pta23.into_push_pull_output();
     let mut pta24 = gpioa.pta24.into_push_pull_output();
+    pta22.set_high().unwrap();
+    pta23.set_high().unwrap();
     pta24.set_high().unwrap();
     loop {}
 }
