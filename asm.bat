b4e1c596871f7c26df24620358fe4584bb5653bc
luojia65 <me@luojia.cc>
2019-11-18 18:30:57 +0800

    Update assemble compile script

--- asm.bat
+++ asm.bat
@@ -1,3 +1,3 @@
-riscv64-unknown-elf-gcc asm.S -o bin/riscv32imc-unknown-none-elf.o -march=rv32imc -mabi=ilp32
+riscv64-unknown-elf-gcc asm.S -o bin/riscv32imc-unknown-none-elf.o -march=rv32imc -mabi=ilp32 -c -g
 wsl ar rcs bin/riscv32imc-unknown-none-elf.a bin/riscv32imc-unknown-none-elf.o
 wsl rm bin/*.o
