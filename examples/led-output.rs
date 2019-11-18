c8f03242762f18931108688eb5a69f88f03d4f2c
luojia65 <me@luojia.cc>
2019-11-18 17:56:24 +0800

    Change example

--- examples/led-output.rs
+++ examples/led-output.rs
@@ -10,11 +10,18 @@ fn main() -> ! {
     let cp = pac::Peripherals::take().unwrap();
     let mut pcc0 = cp.PCC0.constrain();
     let gpioa = (cp.GPIOA, cp.PORTA).split(&mut pcc0.porta).unwrap();
-    let mut pta22 = gpioa.pta22.into_push_pull_output();
-    let mut pta23 = gpioa.pta23.into_push_pull_output();
+    // The red led light is connected to pta24 on vega board.
     let mut pta24 = gpioa.pta24.into_push_pull_output();
-    pta22.set_high().unwrap();
-    pta23.set_high().unwrap();
+    // Do `pta24.set_high()` would set the light on, `set_low` for off.
     pta24.set_high().unwrap();
-    loop {}
+    loop {
+        pta24.toggle().unwrap();
+        delay();
+    }
+}
+
+fn delay() {
+    for _ in 0..800_000 {
+        unsafe { asm!("nop") };
+    }
 }
