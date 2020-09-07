44a6831b9ca7828eb4b5bfb242fef633957b42f8
luojia65 <me@luojia.cc>
2020-09-07 11:41:05 +0800

    Serial LPUART peripheral

--- src/serial.rs
+++ src/serial.rs
@@ -2,40 +2,70 @@
 //! 
 //! This serial module is based on on-chip Low Power Universal Asynchronous Receiver/Transmitter (LPUART).
 use crate::{pac, pcc};
+use core::convert::Infallible;
 
 /// Serial abstraction
 pub struct Serial<LPUART, PINS> {
-    _lpuart: LPUART,
-    _pins: PINS,
+    lpuart: LPUART,
+    pins: PINS,
 }
 
 // todo: change a name
 #[derive(Clone, Copy, Debug)]
-pub enum OpenError {
+pub enum EnableError {
     Absent,
     InUse,
 }
 
-impl<TX, RX> Serial<pac::LPUART0, (TX, RX)> {
+impl<PINS> Serial<pac::LPUART0, PINS> {
     pub fn lpuart0(
         lpuart0: pac::LPUART0,
-        pins: (TX, RX),
+        pins: PINS,
         pcc_lpuart0: &mut pcc::LPUART0,
-    ) -> Result<Self, OpenError> {
+    ) -> Result<Self, EnableError> {
         if !pcc_lpuart0.reg().read().pr().is_pr_1() {
-            return Err(OpenError::Absent)
+            return Err(EnableError::Absent)
         }
         if pcc_lpuart0.reg().read().inuse().is_inuse_1() {
-            return Err(OpenError::InUse)
+            return Err(EnableError::InUse)
         }
-        // todo
-        Ok(Serial { _lpuart: lpuart0, _pins: pins })
+        // enable port clock
+        pcc_lpuart0.reg().write(|w| w.cgc().set_bit());
+        Ok(Serial { lpuart: lpuart0, pins })
+    }
+
+    pub fn release(self, pcc_lpuart: &mut pcc::LPUART0) -> (pac::LPUART0, PINS) {
+        // disable port clock
+        pcc_lpuart.reg().write(|w| w.cgc().clear_bit());
+        // return ownership of peripherals
+        (self.lpuart, self.pins)
     }
 }
 
-/// RX pin - DO NOT IMPLEMENT THIS TRAIT
-pub unsafe trait RxPin<LPUART> {}
+/// Serial pins - DO NOT IMPLEMENT THIS TRAIT
+pub unsafe trait Pins<UART> {}
 
-/// TX pin - DO NOT IMPLEMENT THIS TRAIT
-pub unsafe trait TxPin<LPUART> {}
+impl<PINS> embedded_hal::serial::Write<u8> for Serial<pac::LPUART0, PINS> {
+    /// Write error
+    type Error = Infallible;
 
+    /// Writes a single word to the serial interface
+    fn try_write(&mut self, word: u8) -> nb::Result<(), Self::Error> {
+        todo!("{:?}", word)
+    }
+
+    /// Ensures that none of the previously written words are still buffered
+    fn try_flush(&mut self) -> nb::Result<(), Self::Error> {
+        todo!()
+    }
+}
+
+impl<PINS> embedded_hal::serial::Read<u8> for Serial<pac::LPUART0, PINS> {
+    /// Read error
+    type Error = Infallible;
+
+    /// Reads a single word from the serial interface
+    fn try_read(&mut self) -> nb::Result<u8, Self::Error> {
+        todo!()
+    }
+}
